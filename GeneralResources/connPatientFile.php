8cbcf5ae7ba264db292c4292e0633ad5022e795e
!Me <149876090+ThnsVs@users.noreply.github.com>
2024-08-29 02:16:55 +0300

    Update connPatientFile.php

--- GeneralResources/connPatientFile.php
+++ GeneralResources/connPatientFile.php
@@ -6,7 +6,7 @@
 define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'patientfile0');
+define('DB_NAME', 'patientfile');
 
 $link = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
